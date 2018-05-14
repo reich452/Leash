169d4211540d66bbb2e8366479fcc4f85299ce6b
reich452 <reich452@regis.edu>
2018-05-14 10:14:56 -0600

    Added Theme file

---
+++ Leash/Utilities/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  Leash
+//
+//  Created by Nick Reichard on 5/14/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    /// Only available on the class UIColor. Main purple theme through out
+    static func leashPurple(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: 117.0/255.0, green: 91.0/255.0, blue: 208.0/255.0, alpha: 1.0)
+    }
+}
