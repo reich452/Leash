85bd342fcd789d3180443a8342a16a7cbc2095da
reich452 <reich452@regis.edu>
2018-05-15 11:47:59 -0600

    Added constants file

---
+++ Leash/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Leash
+//
+//  Created by Nick Reichard on 5/14/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+
+enum Constants: String {
+    case placeHolderPath = "_placeholderLabel.textColor"
+}
