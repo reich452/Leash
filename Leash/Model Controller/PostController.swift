df28080b3009cf9bc15c6172a4edb75206ab310f
reich452 <reich452@regis.edu>
2018-05-23 08:11:51 -0600

    Updated ModelController

--- Leash/Model Controller/PostController.swift
+++ Leash/Model Controller/PostController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Nick Reichard. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Firebase
 
 final class PostController {
@@ -17,9 +17,37 @@ final class PostController {
     
     private init() { }
     
-    // MARK: - Firebase
-    
     let dataBaseRef = Database.database().reference()
     let postDataBaseRef = Database.database().reference().child("posts")
     let photoStorage = Storage.storage().reference().child("photos")
+    
+    // MARK: - Firebase URLS
+    private enum FBEndpoints {
+        case Base
+        case Users
+        case User(String)
+        case PostsUrl
+        
+        func url() -> Database {
+            switch self {
+            case .Base:
+                return Database.database(url: "https://leash-ios.firebaseio.com/")
+            case .Users:
+                return Database.database(url: "https://leash-ios.firebaseio.com/users")
+            case .User(let n):
+                return Database.database(url: "https://leash-ios.firebaseio.com/\(n)")
+            case .PostsUrl:
+                return Database.database(url: "https://leash-ios.firebaseio.com/posts")
+            }
+        }
+    }
+    
+    // MARK: - Create
+    
+    typealias uploadCompletionHandler = (_ completion: UIImage?) -> Void
+    
+    func uploadImage(image: UIImage, completion: uploadCompletionHandler) {
+        
+    }
+        
 }
